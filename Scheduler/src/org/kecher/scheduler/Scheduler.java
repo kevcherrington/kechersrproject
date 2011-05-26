5edeb87ba7272f3acf1f483bc68a9ea975f2ccc0
kevcherrington@gmail.com <kevcherrington@gmail.com@57b1b8f4-9f88-12ec-fbbc-1f87fd08f81e>
2011-05-26 02:42:44 +0000

    I created a toast on a 10 second timer!!!

--- Scheduler/src/org/kecher/scheduler/Scheduler.java
+++ Scheduler/src/org/kecher/scheduler/Scheduler.java
@@ -1,13 +1,27 @@
 package org.kecher.scheduler;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 public class Scheduler extends Activity {
+	private Handler mHandler = new Handler();
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mHandler.removeCallbacks(showToast);
+        mHandler.postDelayed(showToast, 10000);
     }
+
+    private Runnable showToast = new Runnable() {
+    	public void run() {
+    		Context context = getApplicationContext();
+    		Toast toast = Toast.makeText(context, "Hello World", Toast.LENGTH_LONG);
+    		toast.show();
+    	}
+    };
 }
