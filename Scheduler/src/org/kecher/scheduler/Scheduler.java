f6cdcd59d063f0f9e3e90a095603883abf6843c0
kevcherrington@gmail.com <kevcherrington@gmail.com@57b1b8f4-9f88-12ec-fbbc-1f87fd08f81e>
2011-06-09 04:08:19 +0000

    clock etc.

--- Scheduler/src/org/kecher/scheduler/Scheduler.java
+++ Scheduler/src/org/kecher/scheduler/Scheduler.java
@@ -1,27 +1,59 @@
 package org.kecher.scheduler;
 
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
 import android.widget.Toast;
 
 public class Scheduler extends Activity {
-	private Handler mHandler = new Handler();
+//	private Handler mHandler = new Handler();
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        mHandler.removeCallbacks(showToast);
-        mHandler.postDelayed(showToast, 10000);
+		scheduleAlarm();
+		silenceRinger();
+		Toast.makeText(getApplicationContext(), "AlarmScheduled",
+				Toast.LENGTH_SHORT).show();
+
+//        mHandler.removeCallbacks(showToast);
+//        mHandler.postDelayed(showToast, 10000);
     }
 
-    private Runnable showToast = new Runnable() {
-    	public void run() {
-    		Context context = getApplicationContext();
-    		Toast toast = Toast.makeText(context, "Hello World", Toast.LENGTH_LONG);
-    		toast.show();
-    	}
-    };
+	private void silenceRinger() {
+		AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+	
+		am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+	}
+
+	private void scheduleAlarm() {
+		// get a Calendar object with current time
+		Calendar cal = Calendar.getInstance();
+		// add 5 minutes to the calendar object
+		cal.add(Calendar.MINUTE, 1);
+		Intent intent = new Intent(getApplicationContext(), AlarmRec.class);
+		intent.putExtra("volume", new Integer(100));
+		intent.putExtra("vibrate", new Integer(1));
+		PendingIntent sender = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		// Get the AlarmManager service
+		AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+		am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), sender);
+	}
+
+//    private Runnable showToast = new Runnable() {
+//    	public void run() {
+//    		Context context = getApplicationContext();
+//    		Toast toast = Toast.makeText(context, "Hello World", Toast.LENGTH_LONG);
+//    		toast.show();
+//    	}
+//    };
 }
