481550b88d22c26b7feb85cb8865ed89364d3028
kevcherrington@gmail.com <kevcherrington@gmail.com@57b1b8f4-9f88-12ec-fbbc-1f87fd08f81e>
2011-06-27 14:51:45 +0000

    updated alot klok.db included

--- Scheduler/gen/org/kecher/scheduler/R.java
+++ Scheduler/gen/org/kecher/scheduler/R.java
@@ -8,6 +8,9 @@
 package org.kecher.scheduler;
 
 public final class R {
+    public static final class array {
+        public static final int ring_modes=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -16,19 +19,41 @@ public final class R {
         public static final int icon=0x7f020002;
     }
     public static final class id {
-        public static final int add_event=0x7f060000;
-        public static final int del_event=0x7f060001;
+        public static final int add_event=0x7f070007;
+        public static final int body=0x7f070001;
+        public static final int confirm=0x7f070005;
+        public static final int del_event=0x7f070008;
+        public static final int mode=0x7f070002;
+        public static final int text1=0x7f070006;
+        public static final int title=0x7f070000;
+        public static final int vibrate=0x7f070004;
+        public static final int volume=0x7f070003;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int event_edit=0x7f030000;
+        public static final int events_list=0x7f030001;
+        public static final int events_row=0x7f030002;
+        public static final int main=0x7f030003;
     }
     public static final class menu {
-        public static final int menu=0x7f050000;
+        public static final int menu=0x7f060000;
     }
     public static final class string {
         public static final int add_event=0x7f040002;
         public static final int app_name=0x7f040001;
+        public static final int confirm=0x7f04000d;
         public static final int del_event=0x7f040003;
+        public static final int edit_event=0x7f04000e;
         public static final int hello=0x7f040000;
+        public static final int menu_delete=0x7f040006;
+        public static final int menu_insert=0x7f040005;
+        public static final int mode=0x7f04000a;
+        public static final int no_events=0x7f040004;
+        public static final int normal=0x7f04000f;
+        public static final int repetition=0x7f040009;
+        public static final int time=0x7f040008;
+        public static final int title=0x7f040007;
+        public static final int vibrate=0x7f04000c;
+        public static final int volume=0x7f04000b;
     }
 }
--- Scheduler/src/org/kecher/scheduler/AlarmRec.java
+++ Scheduler/src/org/kecher/scheduler/AlarmRec.java
@@ -1,35 +1,29 @@
 package org.kecher.scheduler;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.media.AudioManager;
-import android.os.Bundle;
-import android.util.Log;
 
-public class AlarmRec extends BroadcastReceiver {
-	private static final String TAG = "AlarmReceiver";
-
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		try {
-			AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-			Bundle bundle = intent.getExtras();
-		
-			int level = bundle.getInt("volume");
-			int vibrate = bundle.getInt("vibrate");
-			int curLevel = am.getStreamVolume(AudioManager.STREAM_RING);
-			int maxLevel = am.getStreamMaxVolume(AudioManager.STREAM_RING);
-		
-		
-			Log.d(TAG, "max volume level: " + maxLevel + " Current Level: " + curLevel);
-			Log.d(TAG, "percentage: " + (curLevel / maxLevel) * 100);
-		
-			am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-			am.setStreamVolume(AudioManager.STREAM_RING, maxLevel, vibrate);
-		} catch (Exception e) {
-			Log.d(TAG, "didn't work...");
-		}
-	}
+public class AlarmRec { //extends BroadcastReceiver {
+//	private static final String TAG = "AlarmReceiver";
+//
+//	@Override
+//	public void onReceive(Context context, Intent intent) {
+//		try {
+//			AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+//			Bundle bundle = intent.getExtras();
+//		
+//			int level = bundle.getInt("volume");
+//			int vibrate = bundle.getInt("vibrate");
+//			int curLevel = am.getStreamVolume(AudioManager.STREAM_RING);
+//			int maxLevel = am.getStreamMaxVolume(AudioManager.STREAM_RING);
+//		
+//		
+//			Log.d(TAG, "max volume level: " + maxLevel + " Current Level: " + curLevel);
+//			Log.d(TAG, "percentage: " + (curLevel / maxLevel) * 100);
+//		
+//			am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+//			am.setStreamVolume(AudioManager.STREAM_RING, maxLevel, vibrate);
+//		} catch (Exception e) {
+//			Log.d(TAG, "didn't work...");
+//		}
+//	}
 
 }
