dfb240c23ffd742890cae356e38986604be4961c
kevcherrington@gmail.com <kevcherrington@gmail.com@57b1b8f4-9f88-12ec-fbbc-1f87fd08f81e>
2011-07-14 16:41:22 +0000

    Im Pretty much done!!!!! now I have lots of testing :-(

--- Scheduler/src/org/kecher/scheduler/SchedulerReciever.java
+++ Scheduler/src/org/kecher/scheduler/SchedulerReciever.java
@@ -17,7 +17,7 @@ import android.widget.Toast;
 public class SchedulerReciever extends BroadcastReceiver {
 
 	private static final String TAG = "Reciever";
-	private SchedulerService service = new SchedulerService();
+	// private SchedulerService service = new SchedulerService();
 	private EventsDbAdapter mDbAdapter;
 	private ArrayList<Boolean> mWeekDays;
 	private Context mContext;
@@ -25,37 +25,44 @@ public class SchedulerReciever extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		mContext = context;
-		if ((intent.getAction() != null) && 
-				(intent.getAction().equals("android.intent.action.BOOT_COMPLETED"))) {
+		if ((intent.getAction() != null)
+				&& (intent.getAction()
+						.equals("android.intent.action.BOOT_COMPLETED"))) {
 			Intent newIntent = new Intent(context, SchedulerService.class);
 			newIntent.putExtra("message", "The Service is Running");
-			service.startService(newIntent);
+			Log.d(TAG, "If I wanted a service I should start it now.");
+			// service.startService(newIntent);
 		} else {
 			try {
 				mDbAdapter = new EventsDbAdapter(context);
 				mDbAdapter.open();
-				
-				AudioManager adoMngr = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-				
+
+				AudioManager adoMngr = (AudioManager) context
+						.getSystemService(Context.AUDIO_SERVICE);
+
 				Bundle bundle = intent.getExtras();
-				
-				Cursor event = mDbAdapter.fetchEvent(bundle.getLong(EventsDbAdapter.KEY_ROWID));
-				String mode = event.getString(event.getColumnIndexOrThrow(EventsDbAdapter.KEY_MODE));
-				int vol = event.getInt(event.getColumnIndexOrThrow(EventsDbAdapter.KEY_VOL));
-				boolean vibe = (event.getInt(event.getColumnIndexOrThrow(EventsDbAdapter.KEY_SAT)) == 1)
-					? true : false;
-				String[] modes = context.getResources().getStringArray(R.array.ring_modes);
-				
-			
+
+				Cursor event = mDbAdapter.fetchEvent(bundle
+						.getLong(EventsDbAdapter.KEY_ROWID));
+				String mode = event.getString(event
+						.getColumnIndexOrThrow(EventsDbAdapter.KEY_MODE));
+				int vol = event.getInt(event
+						.getColumnIndexOrThrow(EventsDbAdapter.KEY_VOL));
+				boolean vibe = (event.getInt(event
+						.getColumnIndexOrThrow(EventsDbAdapter.KEY_VIBRATE)) == 1) ? true
+						: false;
+				String[] modes = context.getResources().getStringArray(
+						R.array.ring_modes);
+
 				if (mode.equals(modes[2])) { // Silent
 					Log.d(TAG, "Changed to " + mode + " " + vol + " " + vibe);
 					adoMngr.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-//					adoMngr.setVibrateSetting(AudioManager.VIBRATE_TYPE_RINGER,
-//							AudioManager.VIBRATE_SETTING_OFF);
-//					adoMngr.setVibrateSetting(AudioManager.VIBRATE_TYPE_NOTIFICATION,
-//							AudioManager.VIBRATE_SETTING_OFF);
-//					adoMngr.setStreamVolume(AudioManager.STREAM_RING, 0,
-//							AudioManager.FLAG_SHOW_UI);
+					// adoMngr.setVibrateSetting(AudioManager.VIBRATE_TYPE_RINGER,
+					// AudioManager.VIBRATE_SETTING_OFF);
+					// adoMngr.setVibrateSetting(AudioManager.VIBRATE_TYPE_NOTIFICATION,
+					// AudioManager.VIBRATE_SETTING_OFF);
+					// adoMngr.setStreamVolume(AudioManager.STREAM_RING, 0,
+					// AudioManager.FLAG_SHOW_UI);
 					// turn off Ringer and Vibrate
 				} else if (mode.equals(modes[1])) { // Vibrate
 					Log.d(TAG, "Changed to " + mode + " " + vol + " " + vibe);
@@ -64,46 +71,67 @@ public class SchedulerReciever extends BroadcastReceiver {
 				} else if (mode.equals(modes[0])) { // Normal
 					Log.d(TAG, "Changed to " + mode + " " + vol + " " + vibe);
 					adoMngr.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+
+					if (vibe) {
+						adoMngr.setVibrateSetting(AudioManager.VIBRATE_TYPE_RINGER,
+								AudioManager.VIBRATE_SETTING_ON);
+					} else {
+						adoMngr.setVibrateSetting(AudioManager.VIBRATE_TYPE_RINGER,
+								AudioManager.VIBRATE_SETTING_OFF);
+					}
 					// Set the Volume and vibrate indicated.
 				} else {
 					throw new Exception("Ring Scheduler: invalid mode provided");
 				}
-				
+
 				rescheduleEvent(bundle.getLong(EventsDbAdapter.KEY_ROWID));
-				
+
 			} catch (Exception e) {
-				Toast.makeText(context,	"Ring Scheduler: An error occured",
+				Toast.makeText(context, "Ring Scheduler: An error occured",
 						Toast.LENGTH_SHORT).show();
 				e.printStackTrace();
+			} finally { // Make sure to release resources
+				mDbAdapter.close();
 			}
 		}
 	}
-	
+
 	private void rescheduleEvent(Long rowId) {
 		Cursor event = mDbAdapter.fetchEvent(rowId);
-			
+
 		Calendar cal = Calendar.getInstance();
-		
-		cal.set(Calendar.HOUR_OF_DAY, event.getInt(event.getColumnIndexOrThrow(EventsDbAdapter.KEY_RUN_TIME_HOUR)));
-		cal.set(Calendar.MINUTE, event.getInt(event.getColumnIndexOrThrow(EventsDbAdapter.KEY_RUN_TIME_MINUTE)));
+
+		cal.set(Calendar.HOUR_OF_DAY, event.getInt(event
+				.getColumnIndexOrThrow(EventsDbAdapter.KEY_RUN_TIME_HOUR)));
+		cal.set(Calendar.MINUTE, event.getInt(event
+				.getColumnIndexOrThrow(EventsDbAdapter.KEY_RUN_TIME_MINUTE)));
 		cal.set(Calendar.SECOND, 0);
 		cal.set(Calendar.MILLISECOND, 0);
-		
+
+		mWeekDays = new ArrayList<Boolean>();
+
 		mWeekDays.add((event.getInt(event
-				.getColumnIndexOrThrow(EventsDbAdapter.KEY_SUN)) == 1) ? true : false);
+				.getColumnIndexOrThrow(EventsDbAdapter.KEY_SUN)) == 1) ? true
+				: false);
 		mWeekDays.add((event.getInt(event
-				.getColumnIndexOrThrow(EventsDbAdapter.KEY_MON)) == 1) ? true : false);
+				.getColumnIndexOrThrow(EventsDbAdapter.KEY_MON)) == 1) ? true
+				: false);
 		mWeekDays.add((event.getInt(event
-				.getColumnIndexOrThrow(EventsDbAdapter.KEY_TUES)) == 1) ? true : false);
+				.getColumnIndexOrThrow(EventsDbAdapter.KEY_TUES)) == 1) ? true
+				: false);
 		mWeekDays.add((event.getInt(event
-				.getColumnIndexOrThrow(EventsDbAdapter.KEY_WED)) == 1) ? true : false);
+				.getColumnIndexOrThrow(EventsDbAdapter.KEY_WED)) == 1) ? true
+				: false);
 		mWeekDays.add((event.getInt(event
-				.getColumnIndexOrThrow(EventsDbAdapter.KEY_THUR)) == 1) ? true : false);
+				.getColumnIndexOrThrow(EventsDbAdapter.KEY_THUR)) == 1) ? true
+				: false);
 		mWeekDays.add((event.getInt(event
-				.getColumnIndexOrThrow(EventsDbAdapter.KEY_FRI)) == 1) ? true : false);
+				.getColumnIndexOrThrow(EventsDbAdapter.KEY_FRI)) == 1) ? true
+				: false);
 		mWeekDays.add((event.getInt(event
-				.getColumnIndexOrThrow(EventsDbAdapter.KEY_SAT)) == 1) ? true : false);
-		
+				.getColumnIndexOrThrow(EventsDbAdapter.KEY_SAT)) == 1) ? true
+				: false);
+
 		int curDay = cal.get(Calendar.DAY_OF_WEEK);
 		curDay--;
 		for (int i = 0; i < 7; i++) {
@@ -112,18 +140,19 @@ public class SchedulerReciever extends BroadcastReceiver {
 				break;
 			}
 		}
-		
+
 		Intent intent = new Intent(mContext, SchedulerReciever.class);
 		intent.putExtra(EventsDbAdapter.KEY_ROWID, rowId);
-		
-		PendingIntent sender = PendingIntent.getBroadcast(
-				mContext, 123456, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-		
-		AlarmManager am = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+
+		PendingIntent sender = PendingIntent.getBroadcast(mContext, 123456,
+				intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		AlarmManager am = (AlarmManager) mContext
+				.getSystemService(Context.ALARM_SERVICE);
 		am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), sender);
 
-		mDbAdapter.updateEventRunTimes(rowId, (event.getLong(
-				event.getColumnIndexOrThrow(EventsDbAdapter.KEY_NEXT_RUN))),
-				cal.getTimeInMillis());
+		mDbAdapter.updateEventRunTimes(rowId, (event.getLong(event
+				.getColumnIndexOrThrow(EventsDbAdapter.KEY_NEXT_RUN))), cal
+				.getTimeInMillis());
 	}
 }
